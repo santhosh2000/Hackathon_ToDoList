826861a7949a0bd73b6e85fe1776a5dfa42da3e7
ardithamar <ardithamar@gmail.com>
2020-07-11 01:09:47 -0500

    added website

--- todo/tasks/forms.py
+++ todo/tasks/forms.py
@@ -6,6 +6,7 @@ from .models import *
 
 class TaskForm(forms.ModelForm):
 	title= forms.CharField(widget= forms.TextInput(attrs={'placeholder':'Add new task...'}))
+
 	class Meta:
 		model = Task
 		fields = '__all__'
